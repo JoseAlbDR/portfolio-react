fb751ea3f202ed26c659beabaccd7d5b038b2a6e
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-27 16:22:29 +0200

    feat: add social media to SideBar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,7 +3,17 @@ import './Sidebar.scss'
 import LogoJ from '../../assets/images/logo.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import {
+  faCode,
+  faEnvelope,
+  faHome,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons'
+import {
+  faDiscord,
+  faGithub,
+  faLinkedin,
+} from '@fortawesome/free-brands-svg-icons'
 function Sidebar() {
   return (
     <div className="nav-bar">
@@ -26,6 +36,44 @@ function Sidebar() {
           />
         </NavLink>
       </nav>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/jalbertodelgado/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#fff" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/JoseAlbDR"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#fff" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://codesandbox.io/u/josealbdr"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faCode} color="#fff" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://discord.com/app/469452782303313921"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faDiscord} color="#fff" />
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
