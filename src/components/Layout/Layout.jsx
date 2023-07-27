1965d790150df1d0807ddd972c6b2cdc83ddb9f5
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-27 17:51:39 +0200

    style: change some style class names

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,15 +3,14 @@ import Sidebar from '../Sidebar/Sidebar'
 import './Layout.scss'
 function Layout() {
   return (
-    <div className="App">
+    <div className="app">
       <Sidebar />
-      <div className="page">
+      <main className="page">
         <span className="tags top-tags">&lt;body&gt;</span>
         <Outlet />
         <span className="tags bottom-tags">&lt;/body&gt;</span>
-        <br />
-        <span className="bottom-tag-html">&lt;/html&gt;</span>
-      </div>
+        <span className="tags bottom-tag-html">&lt;/html&gt;</span>
+      </main>
     </div>
   )
 }
