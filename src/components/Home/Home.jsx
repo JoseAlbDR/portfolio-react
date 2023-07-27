684ac34b55276d045b2e37bc25eec08face050cc
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-27 19:53:28 +0200

    feat: replace span tags witn AnmiatedLetters custom component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,17 @@
 import './Home.scss'
 import LogoTitle from '../../assets/images/logo.png'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
 
 function Home() {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['.', 'A', 'l', 'b', 'e', 'r', 't', 'o']
+  // prettier-ignore
+  const jobArrayFirst = ["F", "u", "l", "l"," ","S","t","a","c","k"]
+  // prettier-ignore
+  const jobArrayLast = ["W","e","b"," ","D","e","v","e","l","o","p","e","r"]
+
   return (
     <div className="container home-page">
       <div className="text-zone">
@@ -11,10 +20,22 @@ function Home() {
           <span>
             I&apos;m
             <img src={LogoTitle} alt="developer" />
-            .Alberto
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
           </span>
-          <span>Full Stack</span>
-          <span>Web Developer</span>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArrayFirst}
+            idx={15}
+          />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArrayLast}
+            idx={15}
+          />
         </h1>
         <h2>Frontent Developer / Backend Developer</h2>
         <Link to="/contact" className="flat-button">
