243fe35d811db7c1f76b91eb0fafa5db75f6ad7b
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-27 19:28:55 +0200

    feat: styles to Home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,26 @@
 import './Home.scss'
+import LogoTitle from '../../assets/images/logo.png'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div>
-      <h1>HOME</h1>
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <span>Hi,</span>
+          <span>
+            I&apos;m
+            <img src={LogoTitle} alt="developer" />
+            .Alberto
+          </span>
+          <span>Full Stack</span>
+          <span>Web Developer</span>
+        </h1>
+        <h2>Frontent Developer / Backend Developer</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
     </div>
   )
 }
