f74d1c641a79aa7f29cab09e6aa2c742cf162b27
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-27 17:06:03 +0200

    feat: add Outlet component and some spans for design and layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,18 @@
+import { Outlet } from 'react-router-dom'
 import Sidebar from '../Sidebar/Sidebar'
 import './Layout.scss'
 function Layout() {
   return (
-    <>
+    <div className="App">
       <Sidebar />
-    </>
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
+        <Outlet />
+        <span className="tags bottom-tags">&lt;/body&gt;</span>
+        <br />
+        <span className="bottom-tag-html">&lt;/html&gt;</span>
+      </div>
+    </div>
   )
 }
 
