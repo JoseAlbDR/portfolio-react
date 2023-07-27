f56685089ac8264d1b27275e563f4d25a29ca4e8
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-27 15:58:05 +0200

    feat: add styles to Sidebar and navigation items

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,14 +1,31 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './Sidebar.scss'
 import LogoJ from '../../assets/images/logo.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 function Sidebar() {
   return (
     <div className="nav-bar">
       <Link className="logo" to="/">
         <img src={LogoJ} alt="logo" />
-        <img src={LogoSubtitle} alt="jadero" />
+        <img className="sub-logo" src={LogoSubtitle} alt="jadero" />
       </Link>
+      <nav>
+        <NavLink exact="true" to="/">
+          <FontAwesomeIcon icon={faHome} color="#fff" />
+        </NavLink>
+        <NavLink exact="true" to="/about" className="about-link">
+          <FontAwesomeIcon icon={faUser} color="#fff" />
+        </NavLink>
+        <NavLink exact="true" to="/contact" className="contact-link">
+          <FontAwesomeIcon
+            icon={faEnvelope}
+            className="contact-link"
+            color="#fff"
+          />
+        </NavLink>
+      </nav>
     </div>
   )
 }
