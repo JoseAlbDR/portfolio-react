9324a214d26839546927a34fa27dada0366d2448
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-28 15:37:11 +0200

    feat: use newly created components inside Sidebar component

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,79 +1,14 @@
-import { Link, NavLink } from 'react-router-dom'
 import './Sidebar.scss'
-import LogoJ from '../assets/images/logo.png'
-import LogoSubtitle from '../assets/images/logo_sub.png'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-  faCode,
-  faEnvelope,
-  faHome,
-  faUser,
-} from '@fortawesome/free-solid-svg-icons'
-import {
-  faDiscord,
-  faGithub,
-  faLinkedin,
-} from '@fortawesome/free-brands-svg-icons'
+import SideLogo from './SideLogo'
+import MainNav from './MainNav'
+import SocialMedia from './SocialMedia'
+
 function Sidebar() {
   return (
     <div className="nav-bar">
-      <Link className="logo" to="/">
-        <img src={LogoJ} alt="logo" />
-        <img className="sub-logo" src={LogoSubtitle} alt="jadero" />
-      </Link>
-      <nav>
-        <NavLink exact="true" to="/home">
-          <FontAwesomeIcon icon={faHome} color="#fff" />
-        </NavLink>
-        <NavLink exact="true" to="/about" className="about-link">
-          <FontAwesomeIcon icon={faUser} color="#fff" />
-        </NavLink>
-        <NavLink exact="true" to="/contact" className="contact-link">
-          <FontAwesomeIcon
-            icon={faEnvelope}
-            className="contact-link"
-            color="#fff"
-          />
-        </NavLink>
-      </nav>
-      <ul>
-        <li>
-          <a
-            href="https://www.linkedin.com/in/jalbertodelgado/"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <FontAwesomeIcon icon={faLinkedin} color="#fff" />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://github.com/JoseAlbDR"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <FontAwesomeIcon icon={faGithub} color="#fff" />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://codesandbox.io/u/josealbdr"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <FontAwesomeIcon icon={faCode} color="#fff" />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://discord.com/app/469452782303313921"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <FontAwesomeIcon icon={faDiscord} color="#fff" />
-          </a>
-        </li>
-      </ul>
+      <SideLogo />
+      <MainNav />
+      <SocialMedia />
     </div>
   )
 }
