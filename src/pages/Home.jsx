375d8c02613f3af9cf340f73dfa58858db0aabec
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-28 15:45:35 +0200

    feat: clean unused imports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,4 @@
 import './Home.scss'
-import LogoTitle from '../assets/images/logo.png'
-import { Link } from 'react-router-dom'
-import { useEffect, useState } from 'react'
-import AnimatedLetters from '../animations/AnimatedLetters'
 import Logo from '../ui/Logo'
 import Banner from '../ui/Banner'
 
--- src/ui/SocialMedia.jsx
+++ src/ui/SocialMedia.jsx
@@ -4,7 +4,6 @@ import {
   faLinkedin,
 } from '@fortawesome/free-brands-svg-icons'
 import { faCode } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import SocialMediaItem from './SocialMediaItem'
 
 function SocialMedia() {
