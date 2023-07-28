834a5352690490330ac22bcdf5db187e060ca1e7
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-28 23:02:05 +0200

    feat: add new component AboutText

---
+++ src/ui/AboutText.jsx
@@ -0,0 +1,20 @@
+function AboutText() {
+  return (
+    <>
+      <p>
+        ¡Hola! Soy J. Alberto Delgado, un estudiante principiante de desarrollo
+        web. Mi pasión es resolver problemas a través de la programación.
+      </p>
+      <p>
+        Actualmente, me encuentro en un bootcamp de desarrollo web full stack,
+        ampliando mis habilidades en front-end y back-end
+      </p>
+      <p>
+        Mi meta es crecer como profesional en el desarrollo web y aportar mi
+        conocimiento a proyectos futuros.
+      </p>
+    </>
+  )
+}
+
+export default AboutText
