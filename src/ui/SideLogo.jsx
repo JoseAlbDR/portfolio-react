eefb0882e67009cf7b395522ad52e206d616966e
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-28 15:36:41 +0200

    feat: new ui component SideLog to show Logo inside Sidebar

---
+++ src/ui/SideLogo.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+import LogoJ from '../assets/images/logo.png'
+import LogoSubtitle from '../assets/images/logo_sub.png'
+
+function SideLogo() {
+  return (
+    <Link className="logo" to="/">
+      <img src={LogoJ} alt="logo" />
+      <img className="sub-logo" src={LogoSubtitle} alt="jadero" />
+    </Link>
+  )
+}
+
+export default SideLogo
