d102edcb6fe6d3a78793eb32ef66a76e33399263
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-28 09:05:44 +0200

    feat: add BootomTags and TopTags components

---
+++ src/components/Layout/BottomTags.jsx
@@ -0,0 +1,12 @@
+import './Layout.scss'
+
+function BottomTags() {
+  return (
+    <>
+      <span className="tags bottom-tags">&lt;/body&gt;</span>
+      <span className="tags bottom-tag-html">&lt;/html&gt;</span>
+    </>
+  )
+}
+
+export default BottomTags
---
+++ src/components/Layout/TopTags.jsx
@@ -0,0 +1,12 @@
+import './Layout.scss'
+
+function TopTags() {
+  return (
+    <>
+      <span className="tags bottom-tag-html">&lt;html&gt;</span>
+      <span className="tags top-tags">&lt;body&gt;</span>
+    </>
+  )
+}
+
+export default TopTags
