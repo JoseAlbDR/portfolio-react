6b2dff2c6f3ef5e753b1b5ec196e5e0609cf5189
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-28 23:03:30 +0200

    feat: create component About with styles

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,47 @@
+import AnimatedLetters from '../animations/AnimatedLetters'
 import './About.scss'
+import { useAnimation } from '../hooks/useAnimation'
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
+import Cubespinner from '../animations/Cubespinner'
+import AboutText from '../ui/AboutText'
 
 function About() {
+  const { letterClass } = useAnimation(3)
+
+  const faces = [
+    { className: 'face1', icon: faReact, color: '#5ED4F4' },
+    { className: 'face2', icon: faJsSquare, color: '#EFD81D' },
+    { className: 'face3', icon: faHtml5, color: '#F06529' },
+    { className: 'face4', icon: faCss3, color: '#28A4D9' },
+    { className: 'face5', icon: faGitAlt, color: '#EC4D28' },
+    { className: 'face6', icon: faAngular, color: '#DD0031' },
+  ]
+
+  const title = 'About me'.split('')
   return (
     <div className="container about-page">
-      <h1>ABOUT</h1>
+      <div className="text-zone">
+        <h1>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={title}
+            idx={15}
+          />
+        </h1>
+        <div className="about-text">
+          <AboutText />
+        </div>
+      </div>
+      <div className="stage-cube-content">
+        <Cubespinner faces={faces} />
+      </div>
     </div>
   )
 }
