e0b04bb02c24fc16363dabeacba333c06d74b10f
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-29 09:47:13 +0200

    feat: start building Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,20 @@
+import AnimatedLetters from '../animations/AnimatedLetters'
+import { useAnimation } from '../hooks/useAnimation'
 import './Contact.scss'
 function Contact() {
+  const title = 'Contact me'.split('')
+  const { letterClass } = useAnimation(4)
   return (
-    <div>
-      <h1>CONTACT</h1>
+    <div className="container contact-page">
+      <div className="text-zone">
+        <h1>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={title}
+            idx={15}
+          />
+        </h1>
+      </div>
     </div>
   )
 }
