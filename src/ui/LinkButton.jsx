010487400ff6acf976273e8f8474340e1d31cbf0
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-29 15:37:57 +0200

    feat: conditionally render a Link or a button based on props

--- src/ui/LinkButton.jsx
+++ src/ui/LinkButton.jsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom'
 
 function LinkButton({ to, className, children }) {
+  if (!to) return <button className={className}>{children}</button>
+
   return (
     <Link to={to} className={className}>
       {children}
