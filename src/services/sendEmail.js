5b858e8a347ceebf700e5d5ec6e1c1abb6131c27
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-30 00:37:39 +0200

    refactor: sendEmail now is an async function

--- src/services/sendEmail.js
+++ src/services/sendEmail.js
@@ -1,18 +1,19 @@
 import emailjs from '@emailjs/browser'
-import { toast } from 'react-hot-toast'
 
-export const sendEmail = (e, form) => {
+export const sendEmail = async (e, form) => {
   e.preventDefault()
+  console.log(form.current)
 
-  emailjs
-    .sendForm(
+  try {
+    const response = await emailjs.sendForm(
       import.meta.env.VITE_EMAILJS_SERVICE_ID,
       import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
       form.current,
       import.meta.env.VITE_EMAILJS_KEY
     )
-    .then(
-      (result) => toast.success('Email sent.'),
-      (error) => toast.error(error.text)
-    )
+    if (!response.text === 'OK')
+      throw new Error('Something went wrong, try again later.')
+  } catch (error) {
+    throw new Error(error.text)
+  }
 }
