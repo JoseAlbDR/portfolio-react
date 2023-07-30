3de3d36ecc744bf6498e5ed752a0ff0b1f1ef678
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-30 18:41:40 +0200

    feat: use List component to render Cubespinner faces

--- src/animations/Cubespinner.jsx
+++ src/animations/Cubespinner.jsx
@@ -1,18 +1,22 @@
 import CubeFace from './CubeFace'
 import './Cube.scss'
+import List from '../ui/List'
 function Cubespinner({ faces }) {
   return (
     <div className="cubespinner">
-      {faces.map((face, i) => {
-        return (
-          <CubeFace
-            key={i}
-            icon={face.icon}
-            color={face.color}
-            className={face.className}
-          />
-        )
-      })}
+      {
+        <List
+          items={faces}
+          render={(face, i) => (
+            <CubeFace
+              key={i}
+              icon={face.icon}
+              color={face.color}
+              className={face.className}
+            />
+          )}
+        />
+      }
     </div>
   )
 }
