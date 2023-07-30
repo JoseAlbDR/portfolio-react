4459c06eb0a4bff24fd0b6910595c866a9b7b1c8
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-30 02:18:15 +0200

    feat: add SectionTitle to Portfolio page

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,12 +1,18 @@
+import AnimatedLetters from '../animations/AnimatedLetters'
 import LoadingAnimation from '../animations/LoadingAnimation'
+import { useAnimation } from '../hooks/useAnimation'
+import SectionTitle from '../ui/SectionTitle'
+import './Portfolio.scss'
 
 function Portfolio() {
+  const { letterClass } = useAnimation(4)
+  const title = 'Portfolio'.split('')
   return (
     <>
       <LoadingAnimation />
       <div className="container home-page">
         <div className="text-zone">
-          <h1>Portfolio</h1>
+          <SectionTitle title={title} />
         </div>
       </div>
     </>
