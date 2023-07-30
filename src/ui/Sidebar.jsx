3adee01d47531d13ae8373dad89378cd07fde134
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-31 01:53:15 +0200

    feat: add burguer menu in SideBar component

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -2,13 +2,18 @@ import './Sidebar.scss'
 import SideLogo from './SideLogo'
 import MainNav from './MainNav'
 import SocialMedia from './SocialMedia'
+import BurguerMenu from './BurguerMenu'
+import { useState } from 'react'
 
 function Sidebar() {
+  const [showNav, setShowNav] = useState(false)
+  console.log(showNav)
   return (
     <div className="nav-bar">
       <SideLogo />
-      <MainNav />
+      <MainNav showNav={showNav} />
       <SocialMedia />
+      <BurguerMenu showNav={showNav} onSetShowNav={setShowNav} />
     </div>
   )
 }
