091171cad1104c902e01d45a148daff52d4701f4
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-31 16:26:46 +0200

    feat: install material ui, use ClickAwayListener to close burguer menu when clicking outside it

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -4,17 +4,25 @@ import MainNav from './MainNav'
 import SocialMedia from './SocialMedia'
 import BurguerMenu from './BurguerMenu'
 import { useState } from 'react'
+import { ClickAwayListener } from '@mui/material'
 
 function Sidebar() {
   const [showNav, setShowNav] = useState(false)
   console.log(showNav)
+
+  const handleClickAway = () => {
+    setShowNav(false)
+  }
+
   return (
-    <div className="nav-bar">
-      <SideLogo />
-      <MainNav showNav={showNav} />
-      <SocialMedia />
-      <BurguerMenu showNav={showNav} onSetShowNav={setShowNav} />
-    </div>
+    <ClickAwayListener onClickAway={handleClickAway}>
+      <div className="nav-bar">
+        <SideLogo />
+        <MainNav showNav={showNav} />
+        <SocialMedia />
+        <BurguerMenu showNav={showNav} onSetShowNav={setShowNav} />
+      </div>
+    </ClickAwayListener>
   )
 }
 
