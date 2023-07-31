b4a0af788a715bf7e2c89cf9921db002eca4ca98
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-01 00:05:12 +0200

    refactor: move stage-cube-content div to Cubespinner component

--- src/animations/Cubespinner.jsx
+++ src/animations/Cubespinner.jsx
@@ -3,20 +3,22 @@ import './Cube.scss'
 import List from '../ui/AppLayout/List'
 function Cubespinner({ faces }) {
   return (
-    <div className="cubespinner">
-      {
-        <List
-          items={faces}
-          render={(face, i) => (
-            <CubeFace
-              key={i}
-              icon={face.icon}
-              color={face.color}
-              className={face.className}
-            />
-          )}
-        />
-      }
+    <div className="stage-cube-content">
+      <div className="cubespinner">
+        {
+          <List
+            items={faces}
+            render={(face, i) => (
+              <CubeFace
+                key={i}
+                icon={face.icon}
+                color={face.color}
+                className={face.className}
+              />
+            )}
+          />
+        }
+      </div>
     </div>
   )
 }
