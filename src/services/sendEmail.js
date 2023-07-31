af1f45fc5da3dd3448503cf7e165919e686b0851
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-07-31 11:19:38 +0200

    feat: add verifyToken service to sendEmail service to validate token response from server

--- src/services/sendEmail.js
+++ src/services/sendEmail.js
@@ -1,16 +1,24 @@
 import emailjs from '@emailjs/browser'
+import { verifyToken } from './verifyToken'
 
-export const sendEmail = async (formData) => {
+export const sendEmail = async (formData, token) => {
   console.log(formData)
   try {
-    const response = await emailjs.send(
-      import.meta.env.VITE_EMAILJS_SERVICE_ID,
-      import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-      formData,
-      import.meta.env.VITE_EMAILJS_KEY
-    )
-    if (!response.text === 'OK')
-      throw new Error('Something went wrong, try again later.')
+    if (token) {
+      const validToken = await verifyToken(token)
+      if (validToken.success) {
+        const response = await emailjs.send(
+          import.meta.env.VITE_EMAILJS_SERVICE_ID,
+          import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+          formData,
+          import.meta.env.VITE_EMAILJS_KEY
+        )
+        if (!response.text === 'OK')
+          throw new Error('Something went wrong, try again later.')
+      } else {
+        throw new Error('Sorry!! Token was not valid')
+      }
+    }
   } catch (error) {
     throw new Error(error.text)
   }
