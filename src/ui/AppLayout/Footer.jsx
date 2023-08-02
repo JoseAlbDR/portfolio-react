69b0857b5aa325614c4e25b43c9668c30795bb23
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-02 18:50:09 +0200

    feat: new component footer

---
+++ src/ui/AppLayout/Footer.jsx
@@ -0,0 +1,12 @@
+import SocialMedia from '../SideBar/SocialMedia'
+import './Footer.scss'
+
+function Footer() {
+  return (
+    <div className="footer">
+      <SocialMedia />
+    </div>
+  )
+}
+
+export default Footer
