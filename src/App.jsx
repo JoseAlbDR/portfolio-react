e6c1d8127169f5b6374afefda69ff4e1a98c2ed1
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-04 22:19:10 +0200

    feat: add new route to skills page

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { Toaster } from 'react-hot-toast'
 import { QueryClient } from '@tanstack/query-core'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import Skills from './pages/Skills'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -29,6 +30,7 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
           <Route path="portfolio" element={<Portfolio />} />
+          <Route path="skills" element={<Skills />} />
         </Route>
       </Routes>
       <Toaster
