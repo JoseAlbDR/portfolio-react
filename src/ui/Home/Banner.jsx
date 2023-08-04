3c5858d46ed2b7014ba73ef17dfa8bf19176c1c1
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-05 00:36:05 +0200

    refactor: img tag to IMG styled component

--- src/ui/Home/Banner.jsx
+++ src/ui/Home/Banner.jsx
@@ -3,6 +3,18 @@ import LogoTitle from '../../assets/images/logo.png'
 import LinkButton from '../AppLayout/LinkButton'
 import '../../animations/AnimatedLetters.scss'
 import { useAnimation } from '../../hooks/useAnimation'
+import { styled } from 'styled-components'
+
+const H1 = styled.h1``
+
+const IMG = styled.img`
+  width: 38px;
+  margin-left: 15px;
+  opacity: 0;
+  height: auto;
+  animation: rotateIn 1s linear both;
+  animation-delay: 1.4s;
+`
 
 function Banner() {
   const { letterClass } = useAnimation(4)
@@ -12,14 +24,14 @@ function Banner() {
 
   return (
     <>
-      <h1>
+      <H1>
         <span>
           <AnimatedLetters letterClass={letterClass} strArray={'Hi'} idx={12} />
         </span>
         <span>
           <AnimatedLetters letterClass={letterClass} strArray={'I'} idx={13} />
           <AnimatedLetters letterClass={letterClass} strArray={`'m`} idx={14} />
-          <img src={LogoTitle} alt="developer" />
+          <IMG src={LogoTitle} alt="developer" />
           <AnimatedLetters letterClass={letterClass} strArray={name} idx={15} />
         </span>
         <AnimatedLetters
@@ -32,7 +44,7 @@ function Banner() {
           strArray={jobLineTwo}
           idx={15}
         />
-      </h1>
+      </H1>
       <h2>Frontent Developer / Backend Developer</h2>
       <LinkButton to="/contact" className="flat-button">
         CONTACT ME
