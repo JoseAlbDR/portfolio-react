11a394a6abe09e166a9f0280b2b3d488acbe93ba
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-04 22:20:22 +0200

    feat: new folder por skills page content

---
+++ src/ui/Skills/TagCanvas.jsx
@@ -0,0 +1,40 @@
+import TagCloud from '@frank-mayer/react-tag-cloud'
+import './TagCanvas.scss'
+function TagCanvas() {
+  return (
+    <TagCloud
+      options={{
+        radius: 150,
+        maxSpeed: 'fast',
+      }}
+      onClick={(tag, ev) => alert(tag)}
+      onClickOptions={{ passive: true }}
+      // style={{ maxidth: '100%', height: '100%' }}
+    >
+      {[
+        'VSCode',
+        // 'TypeScript',
+        'React',
+        'Redux',
+        'RTK',
+        'React Query',
+        'React Router',
+        'Angular',
+        'SPA',
+        'ESLint',
+        'HTML',
+        'CSS',
+        'JavaScript',
+        'JSON',
+        'EJS',
+        'JQuery',
+        'Supabase',
+        'AWS',
+        'Node',
+        'Express',
+      ]}
+    </TagCloud>
+  )
+}
+
+export default TagCanvas
