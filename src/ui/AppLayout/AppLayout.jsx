176b80775c4a3eb36d3c1b3e3260d639f466d865
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-05 00:31:41 +0200

    refactor: add styled components, BackgroundImage, Content and MainContainer

--- src/ui/AppLayout/AppLayout.jsx
+++ src/ui/AppLayout/AppLayout.jsx
@@ -1,18 +1,49 @@
 import { Outlet } from 'react-router-dom'
+import { SidebarProvider } from '../../context/SidebarContext'
 import Sidebar from '../SideBar/Sidebar'
 import TopTags from './TopTags'
 import BottomTags from './BottomTags'
-import MainContainer from './MainContainer'
 import Main from './Main'
-import './AppLayout.scss'
-import { SidebarProvider } from '../../context/SidebarContext'
 import Footer from './Footer'
+import styled from '@emotion/styled'
+import background from '../../assets/images/nobgbanner.png'
+
+const BackgroundImage = styled.div`
+  background-image: linear-gradient(
+      rgba(36, 42, 46, 0.8),
+      rgba(36, 42, 46, 0.8)
+    ),
+    url(${background});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  position: fixed;
+  height: 100vh;
+  width: 100vw;
+  z-index: -1;
+`
+
+const Content = styled.div`
+  position: absolute;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 100%;
+`
+
+const MainContainer = styled.div`
+  display: grid;
+  grid-template-columns: auto 1fr;
+
+  @media (max-width: 768px) {
+    grid-template-columns: none;
+  }
+`
 
 function AppLayout() {
   return (
     <>
-      <div className="bg"></div>
-      <div className="content">
+      <BackgroundImage></BackgroundImage>
+      <Content>
         <MainContainer>
           <SidebarProvider>
             <Sidebar />
@@ -24,7 +55,7 @@ function AppLayout() {
           </Main>
           <Footer />
         </MainContainer>
-      </div>
+      </Content>
     </>
   )
 }
