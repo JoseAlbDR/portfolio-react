4630bc917f06f8b1adc48ce41f49588e164fe253
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-05 00:34:55 +0200

    refactor: h1 to H1 styled component

--- src/ui/AppLayout/SectionTitle.jsx
+++ src/ui/AppLayout/SectionTitle.jsx
@@ -1,13 +1,16 @@
+import { styled } from 'styled-components'
 import AnimatedLetters from '../../animations/AnimatedLetters'
 import { useAnimation } from '../../hooks/useAnimation'
+
+const H1 = styled.h1``
 
 function SectionTitle({ title }) {
   const { letterClass } = useAnimation(4)
 
   return (
-    <h1>
+    <H1>
       <AnimatedLetters letterClass={letterClass} strArray={title} idx={15} />
-    </h1>
+    </H1>
   )
 }
 
