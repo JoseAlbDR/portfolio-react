f189b130740c9c2c8142d84e7c70514ff80bf9a4
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-04 23:38:46 +0200

    refactor: create styled components OverflowScroll, Page and FlexRow and use them inside Main

--- src/ui/AppLayout/Main.jsx
+++ src/ui/AppLayout/Main.jsx
@@ -1,11 +1,45 @@
-import './Main.scss'
+import { styled } from 'styled-components'
+// import './Main.scss'
+
+const OverflowScroll = styled.div`
+  @media (min-width: 768px) {
+    overflow: scroll;
+    overflow-x: hidden;
+    height: 100dvh;
+  }
+`
+
+const Page = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 10px;
+  padding-left: 2%;
+  height: 100%;
+  position: relative;
+`
+
+const FlexRow = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px 10px;
+  gap: 15px;
+  height: 100%;
+  // justify-content: center;
+
+  span.tags.tag-html {
+    &:last-of-type {
+      padding-bottom: 20px;
+    }
+  }
+`
 function Main({ children }) {
   return (
-    <div className="of-sc">
-      <main className="page">
-        <div className="flex-row">{children}</div>
-      </main>
-    </div>
+    <OverflowScroll>
+      <Page>
+        <FlexRow>{children}</FlexRow>
+      </Page>
+    </OverflowScroll>
   )
 }
 
