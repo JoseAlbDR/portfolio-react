aa7590948fa9f5d88e629b58a9df8863a504ad5e
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-05 00:35:36 +0200

    refactor: TextZone to styled component

--- src/ui/AppLayout/TextZone.jsx
+++ src/ui/AppLayout/TextZone.jsx
@@ -1,5 +1,13 @@
-function TextZone({ children }) {
-  return <div className="text-zone">{children}</div>
-}
+import { styled } from 'styled-components'
+
+const TextZone = styled.div`
+  max-height: 90%;
+`
 
 export default TextZone
+
+// function TextZone({ children }) {
+//   return <div className="text-zone">{children}</div>
+// }
+
+// export default TextZone
