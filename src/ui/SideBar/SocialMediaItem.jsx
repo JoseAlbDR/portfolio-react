b8e6ec39ff350cfdf914221e0cd26d61aab080e8
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-05 00:37:17 +0200

    refactor: li tag to SocialListItem styled component and a tag to SocialLIstLink styled component

--- src/ui/SideBar/SocialMediaItem.jsx
+++ src/ui/SideBar/SocialMediaItem.jsx
@@ -1,12 +1,26 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { styled } from 'styled-components'
+
+const SocialListItem = styled.li``
+
+const SocialListLink = styled.a`
+  padding: 10px 0;
+  display: block;
+  font-size: 22px;
+  line-height: 16px;
+
+  &:hover svg {
+    color: var(--primary-color);
+  }
+`
 
 function SocialMediaItem({ icon, color, href }) {
   return (
-    <li>
-      <a href={href} rel="noreferrer" target="_blank">
+    <SocialListItem>
+      <SocialListLink href={href} rel="noreferrer" target="_blank">
         <FontAwesomeIcon icon={icon} color={color} />
-      </a>
-    </li>
+      </SocialListLink>
+    </SocialListItem>
   )
 }
 
