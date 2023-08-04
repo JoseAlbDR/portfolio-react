473888f5ab5cd52e3df8ed02d275069d6cd7197d
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-08-05 00:32:46 +0200

    refactor: Container component to styled component that acept type prop for home, about, contact, skills, portfolio

--- src/ui/AppLayout/Container.jsx
+++ src/ui/AppLayout/Container.jsx
@@ -1,5 +1,39 @@
-function Container({ children, type }) {
-  return <div className={`container ${type}-page`}>{children}</div>
-}
+import { css, styled } from 'styled-components'
+
+const Container = styled.div`
+  width: 90%;
+  will-change: contents;
+  opacity: 0;
+  z-index: 1;
+  transform-style: preserve-3d;
+  animation: fadeIn 1s forwards;
+  animation-delay: 1s;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  flex: 1;
+  margin: 0 auto;
+
+  ${(props) =>
+    props.type === 'home' &&
+    css`
+      position: relative;
+      // height: 100%;
+      word-wrap: normal;
+    `}
+
+  ${(props) =>
+    props.type === 'about' &&
+    css`
+      display: flex;
+      flex-direction: column;
+      gap: 7%;
+      width: 100%;
+    `}
+`
+
+// function Container({ children, type }) {
+//   return <div className={`container ${type}-page`}>{children}</div>
+// }
 
 export default Container
