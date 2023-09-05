a704033cafb733562c126006f504930658a5d325
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-05 11:33:09 +0200

    refactor: use backend to send contact form message

--- src/services/sendEmail.js
+++ src/services/sendEmail.js
@@ -1,5 +1,5 @@
-import emailjs from '@emailjs/browser'
 import { verifyToken } from './verifyToken'
+import axios from 'axios'
 
 export const sendEmail = async (formData, token) => {
   console.log(formData)
@@ -7,19 +7,13 @@ export const sendEmail = async (formData, token) => {
     if (token) {
       const validToken = await verifyToken(token)
       if (validToken.success) {
-        const response = await emailjs.send(
-          import.meta.env.VITE_EMAILJS_SERVICE_ID,
-          import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-          formData,
-          import.meta.env.VITE_EMAILJS_KEY
-        )
-        if (!response.text === 'OK')
-          throw new Error('Something went wrong, try again later.')
+        await axios.post(`${import.meta.env.VITE_URL}/contact`, formData)
       } else {
         throw new Error('Sorry!! Token was not valid')
       }
     }
   } catch (error) {
-    throw new Error(error.text)
+    console.log(error)
+    throw new Error(error.response.data.msg)
   }
 }
